2556637f4c372bcb39b230c04e9d7aa8a0672d7f
hackerlad101 <harshdodd@gmail.com>
2023-05-15 16:48:24 +0800

    Code Revamp based on Beta-Testing / Feedback

--- Main.js
+++ Main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const messageContainer1 = document.getElementById('msgPart1');
+  const messageContainer = document.getElementById('msg');
 
   $( function() {
     $( "#welcomeDialog" ).dialog({
@@ -13,95 +13,50 @@ $(document).ready(function() {
         Next: function() {
           $( this ).dialog( "close" );  
           const soundtrack = document.getElementById('soundtrack');
-          soundtrack.play();         
-          const disclaimerContainer = document.getElementById('Disclaimer');
-          disclaimerContainer.style.display = 'block';  
-          setTimeout(function() {
-            disclaimerContainer.style.display = 'none';
-            messageContainer1.style.display = 'block';             
-          }, 9000); // 9sec Reading Time                           
+          soundtrack.play();       
+          messageContainer.style.display = 'block';                           
         }
       }
     });
   }); 
 
-  const nextButton1 = document.getElementById('nextBtn1');  
-  const messageContainer2 = document.getElementById('msgPart2');  
+  const nextButton = document.getElementById('nextBtn'); 
+  const dateQuestionContainer = document.getElementById('dateQuestion'); 
 
-  nextButton1.addEventListener('click', () => {
-    messageContainer1.style.display = 'none';       
-    messageContainer2.style.display = 'block'; 
-  });  
-
-  const nextButton2 = document.getElementById('nextBtn2');  
-  const messageContainer3 = document.getElementById('msgPart3');  
-
-  nextButton2.addEventListener('click', () => {
-    messageContainer2.style.display = 'none';       
-    messageContainer3.style.display = 'block'; 
-  });  
-  
-  const nextButton3 = document.getElementById('nextBtn3');  
-  const messageContainer4 = document.getElementById('msgPart4');  
-
-  nextButton3.addEventListener('click', () => {
-    messageContainer3.style.display = 'none';       
-    messageContainer4.style.display = 'block'; 
-  });  
-  
-  const nextButton4 = document.getElementById('nextBtn4');  
-  const mainQuestionContainer = document.getElementById('mainQuestion');  
-
-  nextButton4.addEventListener('click', () => {
-    messageContainer4.style.display = 'none';       
-    mainQuestionContainer.style.display = 'block';   
-  });    
+  nextButton.addEventListener('click', () => {
+    messageContainer.style.display = 'none';       
+    dateQuestionContainer.style.display = 'block';
+  });   
 
   var decision = "";
 
-  const yesButton1 = document.getElementById('yesBtn1');
-  const noButton1 = document.getElementById('noBtn1');
+  const yesButton = document.getElementById('yesBtn');
+  const noButton = document.getElementById('noBtn');
 
-  noButton1.addEventListener('click', () => {
+  noButton.addEventListener('click', () => {
     decision = "No";
-    sendDiscordMessage(decision);    
-    window.alert("No worries. I respect your decision, and I hope this won't change our friendship as I still value it a lot! You can close this page now");
+    sendDiscordMessage(decision);      
+    window.alert("No worries. What do you suggest we do? Text / Call me!");
   });
 
   var selectedDate = "";  
 
-  yesButton1.addEventListener('click', () => {
-    mainQuestionContainer.style.display = 'none';
+  yesButton.addEventListener('click', () => {
+    decision = "Yes";    
+    dateQuestionContainer.style.display = 'none';
 
-    const dateQuestionContainer = document.getElementById('dateQuestion');
-    dateQuestionContainer.style.display = 'block';
-
-    const yesButton2 = document.getElementById('yesBtn2');
-    const noButton2 = document.getElementById('noBtn2');
-
-    noButton2.addEventListener('click', () => {
-      decision = "Yes_No";
-      sendDiscordMessage(decision);      
-      window.alert("Okay no worries. What do you suggest we do? Text me! You can close this page now");
-    });    
-
-    yesButton2.addEventListener('click', () => {
-      dateQuestionContainer.style.display = 'none';
-
-      // Display a Calendar to select the Date 
-      $("#calendarContainer").show();
-      $(function() {
-        $("#datepicker").datepicker({
-          onSelect: function(dateText) {
-            selectedDate = new Date(dateText).toLocaleDateString();
-            const eventButton = document.getElementById('eventBtn');
-            eventButton.addEventListener('click', () => {
-              decision = "Yes";
-              sendDiscordMessage(decision);              
-              confetti.start();
-            });
-          }
-        });
+    // Display a Calendar to select the Date 
+    $("#calendarContainer").show();
+    $(function() {
+      $("#datepicker").datepicker({
+        onSelect: function(dateText) {
+          selectedDate = new Date(dateText).toLocaleDateString();
+          const eventButton = document.getElementById('eventBtn');
+          eventButton.addEventListener('click', () => {
+            sendDiscordMessage(decision);              
+            confetti.start();
+          });
+        }
       });
     });
   });
@@ -114,15 +69,11 @@ $(document).ready(function() {
     var message = "";
 
     if(dec == 'No') {
-      message = "she said no";
-    }
-
-    if(dec == 'Yes_No') {
-      message = "SHE SAID YES!!! But said No to the Zoom Date question";
+      message = "She said no to the zoom date question";
     }
 
     if(dec == 'Yes') {
-      message = `SHE SAID YES!!! Selected Date: ${selectedDate.slice(0,10)}`;
+      message = `SHE SAID YES!! Selected Date: ${selectedDate.slice(0,10)}`;
     }
 
     var payload = { content: message };
